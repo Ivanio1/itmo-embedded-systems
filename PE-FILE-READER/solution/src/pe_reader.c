ba6bf7b0d4bb82af63b6c21c94fb1bd444a31b28
Sobolev Ivan <sobolev1van@yandex.ru>
2023-03-03 20:39:10 +0300

    Add files via upload

--- PE-FILE-READER/solution/src/pe_reader.c
+++ PE-FILE-READER/solution/src/pe_reader.c
@@ -15,12 +15,13 @@
  * @param file Structure of PE file
  */
 void read_data(FILE *in, struct PEFile *file) {
-    //DOS offset
-    fseek(in, 0x3c, SEEK_SET);
-    //Signature offset
+    //Skipping MS-DOS
+    fseek(in, 0x3c, SEEK_SET);//(0x3c - 60)
+    //Offset - contains the offset of PE file
     uint32_t offset;
-    fread(&offset, sizeof(uint32_t), 1, in);
+    fread(&offset, sizeof(offset), 1, in);
     fseek(in, (long)offset, SEEK_SET);
+    //Signature - PE\x0\x0
     fread(&file->signature, sizeof(file->signature), 1, in);
     fread(&file->header, sizeof(file->header), 1, in);
     file->sectionHeaders = malloc(file->header.NumberOfSections * sizeof(struct SectionHeader));
--- PE-FILE-READER/solution/src/printer.c
+++ PE-FILE-READER/solution/src/printer.c
@@ -26,6 +26,15 @@ void print_answer(FILE *in, FILE *out, struct PEFile *peFile, char *name) {
     for (int16_t i = 0; i < peFile->header.NumberOfSections; i++) {
         if (strcmp((char*)peFile->sectionHeaders[i].name, name) == 0) {
             struct SectionHeader section_header = peFile->sectionHeaders[i];
+            printf("%X ", section_header.Misc.VirtualSize);
+            printf("%X ", section_header.VirtualAddress);
+            printf("%X ", section_header.PointerToRawData);
+            printf("%X ", section_header.SizeOfRawData);
+            printf("%X ", section_header.PointerToRelocations);
+            printf("%X ", section_header.NumberOfRelocations);
+            printf("%X ", section_header.PointerToLinenumbers);
+            printf("%X ", section_header.NumberOfLinenumbers);
+            printf("%X", section_header.Characteristics);
             fseek(in, (long) section_header.PointerToRawData, SEEK_SET);
             char *section = malloc(section_header.SizeOfRawData);
             fread(section, section_header.SizeOfRawData, 1, in);
